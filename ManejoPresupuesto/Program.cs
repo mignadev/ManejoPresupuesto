ea074d77035095030196ad5118f19d1064b7ee10
migna <migna@MARCELO>
2026-03-04 13:36:20 -0300

    Agregar archivos de proyecto.

---
+++ ManejoPresupuesto/Program.cs
@@ -0,0 +1,97 @@
+using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using System.Globalization;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var politicaUsuariosAutenticados = new AuthorizationPolicyBuilder()
+    .RequireAuthenticatedUser().Build();
+
+builder.Services.AddControllersWithViews(opciones =>
+{
+    opciones.Filters.Add(new AuthorizeFilter(politicaUsuariosAutenticados));
+});
+builder.Services.AddTransient<IRepositorioTiposCuentas, RepositorioTiposCuentas>();
+builder.Services.AddTransient<IServicioUsuarios, ServicioUsuarios>();
+builder.Services.AddTransient<IRepositorioCuentas, RepositorioCuentas>();
+builder.Services.AddTransient<IRepositorioCategorias, RepositorioCategorias>();
+builder.Services.AddTransient<IRepositorioTransacciones, RepositorioTransacciones>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddTransient<IServicioReportes, ServicioReportes>();
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddTransient<IRepositorioUsuarios, RepositorioUsuarios>();
+builder.Services.AddTransient<IUserStore<Usuario>, UsuarioStore>();
+builder.Services.AddTransient<SignInManager<Usuario>>();
+builder.Services.AddIdentityCore<Usuario>(opciones =>
+{
+    opciones.Password.RequireDigit = false;
+    opciones.Password.RequireLowercase = false;
+    opciones.Password.RequireUppercase = false;
+    opciones.Password.RequireNonAlphanumeric = false;
+}).AddErrorDescriber<MensajesDeErrorIdentity>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultChallengeScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultSignOutScheme = IdentityConstants.ApplicationScheme;
+}).AddCookie(IdentityConstants.ApplicationScheme, opciones =>
+{
+    opciones.LoginPath = "/usuarios/login";
+});
+
+builder.Services.AddTransient<IServicioEmail, ServicioEmail>();
+
+var app = builder.Build();
+
+// 1. Define la cultura que quieres usar ("en-US").
+var culturaEEUU = new CultureInfo("en-US");
+
+// 2. Modifica la configuración de formato de número.
+//    Esto fuerza el uso de 2 decimales para números y moneda.
+culturaEEUU.NumberFormat.NumberDecimalDigits = 2;
+culturaEEUU.NumberFormat.CurrencyDecimalDigits = 2;
+
+// 3. Configura las opciones de localización usando la cultura modificada.
+var opcionesDeLocalizacion = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture(culturaEEUU),
+    SupportedCultures = new[] { culturaEEUU },
+    SupportedUICultures = new[] { culturaEEUU }
+};
+
+// 4. Aplica el middleware de localización.
+//    ¡Asegúrate de que esta línea esté antes de app.UseRouting(), app.UseEndpoints(), etc.!
+app.UseRequestLocalization(opcionesDeLocalizacion);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Transacciones}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
---
+++ ManejoPresupuesto/Servicios/ServicioEmail.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IServicioEmail
+    {
+        Task EnviarEmailCambioPassword(string receptor, string enlace);
+    }
+
+    public class ServicioEmail : IServicioEmail
+    {
+        private readonly IConfiguration configuration;
+
+        public ServicioEmail(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task EnviarEmailCambioPassword(string receptor, string enlace)
+        {
+            var email = configuration.GetValue<string>("CONFIGURACIONES_EMAIL:EMAIL");
+            var password = configuration.GetValue<string>("CONFIGURACIONES_EMAIL:PASSWORD");
+            var host = configuration.GetValue<string>("CONFIGURACIONES_EMAIL:HOST");
+            var puerto = configuration.GetValue<int>("CONFIGURACIONES_EMAIL:PUERTO");
+
+            var cliente = new SmtpClient(host, puerto);
+            cliente.EnableSsl = true;
+            cliente.UseDefaultCredentials = false;
+
+            cliente.Credentials = new NetworkCredential(email, password);
+            var emisor = email;
+            var subject = "Ha olivado su contraseña?";
+            var contenidoHtml = $@"Saludos, 
+Este mensaje le llega porque usted ha solicitado un cambio de contraseña. Si esta solicitud no fue hecha por usted, puede ignorar este mensaje.
+Para cambiar su contraseña, haga click en el siguiente enlace:
+
+{enlace}
+
+Atentamente,
+Equipo Manejo Presupuesto
+";
+
+            var mensaje = new MailMessage(emisor, receptor, subject, contenidoHtml);
+            await cliente.SendMailAsync(mensaje);
+        }
+    }
+}
